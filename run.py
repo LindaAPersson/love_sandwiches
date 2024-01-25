d57416a0c8db5c72c4a55b130c52021be0c7f3e8
Linda Persson <lindapersson-@live.se>
2024-01-25 11:00:42 +0000

    <feat>[input error]added try exception on inputvalue

--- run.py
+++ run.py
@@ -21,6 +21,23 @@ def get_sales_data():
     print('Example: 10, 20, 30, 40, 50, 60\n')
     
     data_str = input('Enter your data here: ')
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all strub values to intregers.
+    Raises valueError if string cannot be converted tinto int,
+    or if ther arent exactrly 6 values.
+    """
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                f"Exactrly 6 values requierd, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f'Invalide data: {e}, please try again. \n')
+
 
 get_sales_data()
