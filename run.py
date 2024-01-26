94248435c3caafa8f00c23d48c91f53b0424950a
Linda Persson <lindapersson-@live.se>
2024-01-26 08:24:40 +0000

    <feat>added stock worksheet uppdate

--- run.py
+++ run.py
@@ -105,6 +105,24 @@ def get_last_5_entries_sales():
     
     return columns
 
+def calculate_stock_data(data):
+    """
+    calculating stock data and addding 10%
+    """
+    print('Calculating stock data')
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column)/len(int_column)
+        #if you know how many items you want you can do 
+        # average = sum(int_column) = / 5 (5 is an exampel)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
+
 def main():
     """
     Run all program functions
@@ -114,7 +132,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 
 print('welcome!')
-# main()
-sales_columns = get_last_5_entries_sales()
+main()
