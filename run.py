3713fe89379f366c68ce18ca2a7503bb31085421
Linda Persson <lindapersson-@live.se>
2024-01-26 07:26:49 +0000

    <feat>refactored sorksheet coe

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET =GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     get sales figures input from the user
@@ -50,14 +51,31 @@ def validate_data(values):
 
     return True
 
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
+"""
+Reminder on how to refactor code that are alike
+
 def update_sales_worksheet(data):
-    """
-    Update sales worksheet
-    """
     print('updateing')
     sales_worksheet = SHEET.worksheet('sales')
     sales_worksheet.append_row(data)
     print('update sales woksheet')
+
+def update_surplus_worksheet(data):
+    print('updating surplus')
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(data)
+    print('surplus updated')
+"""
 
 def calculate_surplus_data(sales_row):
     """
@@ -74,24 +92,15 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
-def update_surplus_worksheet(data):
+def main():
     """
-    update surplus worksheet
-    """
-    print('updating surplus')
-    surplus_worksheet = SHEET.worksheet('surplus')
-    surplus_worksheet.append_row(data)
-    print('surplus updated')
-
-def main(): 
-    """
-    All program function
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 print('welcome!')
 main()
